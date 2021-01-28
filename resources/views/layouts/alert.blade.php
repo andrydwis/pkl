71b9b05d468a755be6a130f65eac066a5562291d
errygautama <errygautama@gmail.com>
2021-01-28 08:38:32 +0700

    untuk demo

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -1,19 +1,18 @@
-@if(session()->has('token'))
-<div class="alert alert-success" role="alert">
-    <h4 class="alert-heading">Terima Kasih Telah Menggunakan Layanan Kami</h4>
-    <p>
-        Token Survey :
+@if (session()->has('token'))
+    <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Terima Kasih Telah Menggunakan Layanan Kami</h4>
+        <p>
+            Token Survey :
         <div class="input-group">
-            <input id="token" type="text" readonly="true" class="form-control"  value="{{session()->get('token')}}">
+            <input id="token" type="text" readonly="true" class="form-control" value="{{ session()->get('token') }}">
             <div class="input-group-append">
-            <button 
-                onclick="CopyFunction()" onmouseout="outFunc()" class="btn btn-primary" type="button">
-                <span class="tooltiptext" id="myTooltip">Copy</span>                
-            </button>
+                <button onclick="CopyFunction()" onmouseout="outFunc()" class="btn btn-primary" type="button">
+                    <span class="tooltiptext" id="myTooltip">Copy</span>
+                </button>
             </div>
         </div>
-    </p>
-    <hr>
-    <p class="mb-0">Anda dapat melakukan survey dengan token tersebut <strong><a href="{{route('survey.index')}}" class="text-success">Disini</a></strong></p>
-</div>
+        </p>
+        <hr>
+        <p class="mb-0">Anda dapat melakukan survey dengan token tersebut <strong>Dibawah ini</strong></p>
+    </div>
 @endif
